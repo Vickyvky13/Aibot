cebe901a14ecb7cdd9a4acda81e8b31d1e49508a
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-04-12 22:37:19 +0530

    Update buttons.py

--- Chatgpt/modules/buttons.py
+++ Chatgpt/modules/buttons.py
@@ -25,7 +25,7 @@ START = f""" <b>
 ──────────────────
 Rᴇᴀᴅ Tʜᴇ ʜᴇʟᴘ sᴇᴄᴛɪᴏɴ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ
 
-Pᴏᴡᴇʀᴇᴅ Bʏ @Mr_Sukkun  </b>"""
+Pᴏᴡᴇʀᴇᴅ Bʏ @solotreee </b>"""
 SOURCE_TEXT = f"""<b>
 ๏ ʜᴇʏ, ɪ ᴀᴍ <a href="https://t.me/{Mukesh.username}"> {Mukesh.name} </a>
 ──────────────────
